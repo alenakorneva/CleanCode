76a6c9d29657420c72e2cb6516f74c6cf4f13f3c
Alena Korneva <ekorneva323@gmail.com>
2021-12-16 20:39:37 +0300

    add corrected files from Airport project

---
+++ src/main/java/Planes/NewPlane.java
@@ -0,0 +1,9 @@
+package Planes;
+
+public class NewPlane extends Plane {
+    public NewPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity) {
+        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
+    }
+
+
+}
